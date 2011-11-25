5a3b4af6455032ba7d5e86c3a7f132200e0e88fc
Sunng <classicning@gmail.com>
2011-11-25 17:30:36 +0800

    separate sync and async

--- src/slacker/client.clj
+++ src/slacker/client.clj
@@ -4,15 +4,25 @@
   (:use [aleph.tcp])
   (:import [slacker SlackerException]))
 
-(defn- call-remote [conn func-name params]
+(defn- handle-response [code data]
+  (cond
+   (= code result-code-success) (read-carb (first data))
+   (= code result-code-notfound) (throw (SlackerException. "not found"))
+   (= code result-code-exception) (throw (SlackerException. (read-carb (first data))))
+   :else (throw (SlackerException. (str "invalid result code: " code)))))
+
+(defn- sync-call-remote [conn func-name params]
   (let [ch (wait-for-result conn)]
     (enqueue ch [version type-request func-name (write-carb params)])
-    (if-let [[version type code data] (wait-for-message ch *timeout*)]
-      (cond
-        (= code result-code-success) (read-carb (first data))
-        (= code result-code-notfound) (throw (SlackerException. "not found"))
-        (= code result-code-exception) (throw (SlackerException. (read-carb (first data))))
-        :else (throw (SlackerException. (str "invalid result code: " code)))))))
+    (if-let [[version type code data] (wait-for-message ch)]
+      (handle-response code data))))
+
+(defn- async-call-remote [conn func-name params]
+  (run-pipeline conn
+    (fn [ch]
+      (enqueue ch [version type-request func-name (write-carb params)])
+      (if-let [[version type code data] (read-channel ch *timeout*)]
+        (handle-response code data)))))
   
 (defn slackerc [host port]
   (tcp-client {:host host
@@ -20,9 +30,14 @@
                :encoder slacker-request-codec
                :decoder slacker-response-codec}))
 
-(defn with-slackerc [conn remote-call-info]
+(defn with-slackerc
+  [conn remote-call-info
+   & {:keys [async]
+      :or {async false}}]
   (let [[fname args] remote-call-info]
-    (call-remote conn fname args)))
+    (if async
+      (async-call-remote conn fname args)
+      (sync-call-remote conn fname args))))
 
 (defmacro defremote [fname]
   `(defn ~fname [& args#]
--- src/slacker/example/client.clj
+++ src/slacker/example/client.clj
@@ -14,7 +14,7 @@
 (defn -main [& args]
   (println (with-slackerc conn (timestamp)))
   (println (with-slackerc conn (inc-m 100)))
-  (println (with-slackerc conn (inc-m 200)))
+  (println (with-slackerc conn (inc-m 200) :async true))
   (println (with-slackerc conn (get-m)))
   (println (with-slackerc conn (rand-ints 10)))
   (try
