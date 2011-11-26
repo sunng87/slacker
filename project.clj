c59c014ecdf5ad2c51b8c7d6eeac3ddaf7fb1aaa
Sun Ning <classicning@gmail.com>
2011-11-26 22:01:23 +0800

    update namespace

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject slacker "0.1.0-SNAPSHOT"
+(defproject info.sunng/slacker "0.1.0-SNAPSHOT"
   :description "Clojure RPC"
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [aleph "0.2.0-rc2"]
