0ca3f864cdac33fbed93e64b770844a036ed0642
Sun Ning <classicning@gmail.com>
2011-12-01 20:20:19 +0800

    remove snapshot in dependency

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "Clojure RPC"
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [aleph "0.2.0"]
-                 [info.sunng/carbonite "0.1.0-SNAPSHOT"]]
+                 [info.sunng/carbonite "0.1.0"]]
   :dev-dependencies [[codox "0.2.3"]]
   :aot [slacker.SlackerException])
 
