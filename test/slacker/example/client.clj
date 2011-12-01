1f841898d63bfb23ea2e4c617d712764f153db7a
Sunng <classicning@gmail.com>
2011-12-01 10:37:17 +0800

    update example for new options of defremote

--- test/slacker/example/client.clj
+++ test/slacker/example/client.clj
@@ -5,7 +5,11 @@
 (def conn (slackerc "localhost" 2104))
 (defremote conn timestamp)
 (defremote conn inc-m)
+(defremote conn get-m :async true)
 (defremote conn show-m :remote-name "get-m")
+(defremote conn get-m2
+  :remote-name "get-m"
+  :callback #(prn "Async get-m ==> " %))
 (defremote conn rand-ints)
 (defremote conn make-error)
 (defremote conn first-arg)
@@ -14,6 +18,8 @@
   (println (timestamp))
   (println (inc-m 100))
   (println (show-m))
+  (println @(get-m))
+  (get-m2)
   (println (rand-ints 10))
   (try
     (make-error)
