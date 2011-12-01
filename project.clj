a87493ba006a170fe5f1032f2f941bb65ecad23d
Sunng <classicning@gmail.com>
2011-12-01 16:22:55 +0800

    upgrade aleph to 0.2.0

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject info.sunng/slacker "0.1.0-SNAPSHOT"
   :description "Clojure RPC"
   :dependencies [[org.clojure/clojure "1.2.1"]
-                 [aleph "0.2.0-rc2"]
+                 [aleph "0.2.0"]
                  [info.sunng/carbonite "0.1.0-SNAPSHOT"]]
   :dev-dependencies [[codox "0.2.3"]]
   :aot [slacker.SlackerException])
