0433a8f31c3b6c016f1be60951785b04f0f55685
Sunng <classicning@gmail.com>
2011-12-05 13:57:08 +0800

    refactoring client

--- src/slacker/client.clj
+++ src/slacker/client.clj
@@ -11,30 +11,37 @@
    (= code result-code-exception) (throw (SlackerException. (read-carb (first data))))
    :else (throw (SlackerException. (str "invalid result code: " code)))))
 
-(defn- sync-call-remote [conn func-name params]
-  (let [request [version type-request content-type-carb func-name (write-carb params)]
-        response (wait-for-result (conn request))]
-    (when-let [[_ _ _ code data] response]
-      (handle-response code data))))
+(defprotocol SlackerClientProtocol
+  (sync-call-remote [this func-name params])
+  (async-call-remote [this func-name params cb]))
 
-(defn- async-call-remote [conn func-name params cb]
-  (let [result-promise (promise)]
-    (run-pipeline
-     (conn [version type-request content-type-carb func-name (write-carb params)])
-     #(if-let [[_ _ _ code data] %]
-        (let [result (handle-response code data)]
-          (deliver result-promise result)
-          (if-not (nil? cb)
-            (cb result)))))
-    result-promise))
+(deftype SlackerClient [host port conn content-type]
+  SlackerClientProtocol
+  (sync-call-remote [this func-name params]
+    (let [request [version type-request content-type func-name (write-carb params)]
+          response (wait-for-result (conn request) *timeout*)]
+      (when-let [[_ _ _ code data] response]
+        (handle-response code data))))
+  (async-call-remote [this func-name params cb]
+    (let [result-promise (promise)]
+      (run-pipeline
+       (conn [version type-request content-type func-name (write-carb params)])
+       #(if-let [[_ _ _ code data] %]
+          (let [result (handle-response code data)]
+            (deliver result-promise result)
+            (if-not (nil? cb) (cb result)))))
+      result-promise)))
 
 (defn slackerc
   "Create connection to a slacker server."
-  [host port]
-  (client #(tcp-client {:host host
-                                  :port port
-                                  :encoder slacker-request-codec
-                                  :decoder slacker-response-codec})))
+  [host port
+   & {:keys [content-type]
+      :or {content-type content-type-carb}}]
+  (let [conn (client #(tcp-client {:host host
+                                   :port port
+                                   :encoder slacker-request-codec
+                                   :decoder slacker-response-codec}))]
+    (SlackerClient. host port conn content-type)))
 
 (defn close-slackerc
   "Close the connection"
@@ -45,13 +52,13 @@
   "Invoke remote function with given slacker connection.
   A call-info tuple should be passed in. Usually you don't use this
   function directly. You should define remote call facade with defremote"
-  [conn remote-call-info
+  [sc remote-call-info
    & {:keys [async callback]
       :or {async false callback nil}}]
   (let [[fname args] remote-call-info]
     (if (or async (not (nil? callback)))
-      (async-call-remote conn fname args callback)
-      (sync-call-remote conn fname args))))
+      (async-call-remote sc fname args callback)
+      (sync-call-remote sc fname args))))
 
 (defmacro defremote
   "Define a facade for remote function. You have to provide the
