bf8e6e6ed0d5d8841070d1f83b84906f8fbe1c81
Sun Ning <classicning@gmail.com>
2011-12-08 21:06:47 +0800

    update performance test script

--- scripts/performance-test.clj
+++ scripts/performance-test.clj
@@ -1,15 +1,24 @@
 (use 'slacker.client)
+(import '[java.util.concurrent Executors TimeUnit])
 
-(def sc (slackerc "localhost" 2104))
-(def scp (slackerc-pool "localhost" 2104))
-
-(defremote sc rand-ints2 :remote-name "rand-ints")
-(defremote scp rand-ints)
 
 (def total-calls (Integer/valueOf (second *command-line-args*)))
-(println (str "Performing " total-calls " requests on the server"))
-(time (dorun (pmap rand-ints (take total-calls (repeat 5)))))
+(def total-threads (Integer/valueOf (nth *command-line-args* 2)))
+(def total-connections (Integer/valueOf (nth *command-line-args* 3)))
+(println (str "Performing " total-calls " requests with "
+              total-threads " threads"))
 
-;(time (dorun (map rand-ints (take total-calls (repeat 5)))))
+(def thread-pool (Executors/newFixedThreadPool total-threads))
+
+(def scp (slackerc-pool "localhost" 2104
+                        :max-active total-connections))
+(defremote scp rand-ints)
+
+(time
+ (do
+   (.invokeAll thread-pool (take total-calls (repeat (fn [] (rand-ints 5)))))
+   (.shutdown thread-pool)
+   (.awaitTermination thread-pool -1 TimeUnit/SECONDS)))
+
 (System/exit 0)
 
