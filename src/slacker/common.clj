f08865300ca7f14490a0d901ee0f7f369dbc7c7d
Sunng <classicning@gmail.com>
2011-12-09 12:53:55 +0800

    update docs

--- src/slacker/common.clj
+++ src/slacker/common.clj
@@ -1,7 +1,13 @@
 (ns slacker.common)
 
-(def *debug* false)
-(def *timeout* 10000)
-(def version (short 2))
+(def
+  ^{:doc "Debug flag. This flag can be override by binding if you like to see some debug output."}
+  *debug* false)
+(def
+  ^{:doc "Timeout for synchronouse call."}
+  *timeout* 10000)
+(def
+  ^{:doc "Protocol version."}
+  version (short 2))
 
 
--- test/slacker/test/client.clj
+++
@@ -1,6 +0,0 @@
-(ns slacker.test.client
-  (:use [lamina.core])
-  (:use [slacker common client])
-  (:use [clojure.test]))
-
-
