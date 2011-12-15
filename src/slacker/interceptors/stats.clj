63fd12b56704236d758f02973d8571c7ce545cbb
Sunng <classicning@gmail.com>
2011-12-15 13:35:26 +0800

    improve stats interceptor

--- src/slacker/interceptors/stats.clj
+++ src/slacker/interceptors/stats.clj
@@ -1,6 +1,8 @@
 (ns slacker.interceptors.stats
   (:require [clojure.contrib.jmx :as jmx])
-  (:import [clojure.contrib.jmx Bean]))
+  (:import [clojure.contrib.jmx Bean])
+  (:import [javax.management DynamicMBean MBeanInfo
+            MBeanAttributeInfo Attribute AttributeList]))
 
 (def stats-data (atom {}))
 
@@ -13,17 +15,32 @@
       (swap! stats-data assoc-with-default fname)))
   req)
 
-(defn new-mbean [state-ref]
-  (proxy [Bean] [state-ref]
-    (getAttribute [attr] 
-       (let [attr-value (@(.state ^clojure.contrib.jmx.Bean this) (keyword attr))]
-         (if (fn? attr-value)
-           (attr-value)
-           attr-value)))))
-
 (def stats-bean
-  (new-mbean
-   (ref {:stats (fn [] (hash-map @stats-data))})))
+  (reify
+    DynamicMBean
+    (getAttribute [this key]
+      (@stats-data key))
+    (getAttributes [this keys]
+      (AttributeList.
+       (apply list (map #(Attribute. % (get @stats-data %)) keys))))
+    (getMBeanInfo [this]
+      (MBeanInfo. (.. @stats-data getClass getName)
+                  "slacker function call statistics"
+                  (into-array
+                   MBeanAttributeInfo
+                   (map #(MBeanAttributeInfo. %
+                                              "java.lang.Integer"
+                                              "function invocation count"
+                                              true
+                                              false
+                                              false)
+                        (keys @stats-data)))
+                  nil
+                  nil
+                  nil))
+    (invoke [this action params sig])
+    (setAttribute [this attr])
+    (setAttributes [this attrs])))
 
 (jmx/register-mbean stats-bean "slacker.server:type=FunctionCallStats")
 
