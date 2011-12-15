c391e6cbe1933fb55e8fdc570e313cdff81af99f
Sunng <classicning@gmail.com>
2011-12-15 12:20:38 +0800

    add stats interceptor

--- examples/slacker/example/server.clj
+++ examples/slacker/example/server.clj
@@ -1,5 +1,6 @@
 (ns slacker.example.server
   (:use slacker.server)
+  (:use slacker.interceptors.stats)
   (:require [slacker.example.api]))
 
 (defn log-function-calls [req]
@@ -8,6 +9,6 @@
 
 (defn -main [& args]
   (start-slacker-server (the-ns 'slacker.example.api) 2104
-                        :before (interceptors log-function-calls))
+                        :before (interceptors log-function-calls function-call-stats))
   (println "Slacker example server started on port 2104."))
 
---
+++ src/slacker/interceptors/stats.clj
@@ -0,0 +1,29 @@
+(ns slacker.interceptors.stats
+  (:require [clojure.contrib.jmx :as jmx])
+  (:import [clojure.contrib.jmx Bean]))
+
+(def stats-data (atom {}))
+
+(defn assoc-with-default [m k]
+  (update-in m [k] #(if (nil? %) 1 (inc %))))
+
+(defn function-call-stats [req]
+  (when (nil? (:code req))
+    (let [fname (:fname req)]
+      (swap! stats-data assoc-with-default fname)))
+  req)
+
+(defn new-mbean [state-ref]
+  (proxy [Bean] [state-ref]
+    (getAttribute [attr] 
+       (let [attr-value (@(.state ^clojure.contrib.jmx.Bean this) (keyword attr))]
+         (if (fn? attr-value)
+           (attr-value)
+           attr-value)))))
+
+(def stats-bean
+  (new-mbean
+   (ref {:stats (fn [] @stats-data)})))
+
+(jmx/register-mbean stats-bean "slacker.server:type=FunctionCallStats")
+
