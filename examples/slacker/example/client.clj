cc51d19746aa2f3e8cb03f6a3a84ede79e1369ed
Sun Ning <classicning@gmail.com>
2011-12-30 20:22:57 +0800

    rename defn-remote option :async to :async?

--- examples/slacker/example/client.clj
+++ examples/slacker/example/client.clj
@@ -6,7 +6,7 @@
 (def conn2 (slackerc "localhost" 2104 :content-type :json))
 (defn-remote conn timestamp)
 (defn-remote conn2 inc-m)
-(defn-remote conn get-m :async true)
+(defn-remote conn get-m :async? true)
 (defn-remote conn2 show-m :remote-name "get-m")
 (defn-remote conn get-m2
   :remote-name "get-m"
--- src/slacker/client.clj
+++ src/slacker/client.clj
@@ -107,22 +107,22 @@
   A call-info tuple should be passed in. Usually you don't use this
   function directly. You should define remote call facade with defremote"
   [sc remote-call-info
-   & {:keys [async callback]
-      :or {async false callback nil}}]
+   & {:keys [async? callback]
+      :or {async? false callback nil}}]
   (let [[fname args] remote-call-info]
-    (if (or async (not (nil? callback)))
+    (if (or async? (not (nil? callback)))
       (async-call-remote sc fname args callback)
       (sync-call-remote sc fname args))))
 
 (defmacro defn-remote
   "Define a facade for remote function. You have to provide the
   connection and the function name. (Argument list is not required here.)"
-  [sc fname & {:keys [remote-name async callback]
-               :or {remote-name nil async false callback nil}}]
+  [sc fname & {:keys [remote-name async? callback]
+               :or {remote-name nil async? false callback nil}}]
   `(defn ~fname [& args#]
      (with-slackerc ~sc
        [(or ~remote-name (name '~fname))
         (into [] args#)]
-       :async ~async
+       :async? ~async?
        :callback ~callback)))
 
