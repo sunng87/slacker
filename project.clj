3780bb8c805cd207a439add3919bcf29a3d75b0d
Sun Ning <classicning@gmail.com>
2012-01-10 20:47:55 +0800

    start next development iteration

--- project.clj
+++ project.clj
@@ -1,14 +1,13 @@
-(defproject slacker "0.4.0"
+(defproject slacker "0.5.0-SNAPSHOT"
   :description "Clojure RPC based on serialization and TCP"
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [aleph "0.2.0"]
                  [info.sunng/carbonite "0.1.1"]
                  [clj-json "0.4.3"]
                  [commons-pool/commons-pool "1.5.6"]
-                 [slingshot "0.10.0"]]
+                 [slingshot "0.10.1"]]
   :dev-dependencies [[codox "0.3.3"]
-                     [lein-exec "0.1"]
-                     [lein-sub "0.1.1"]]
+                     [lein-exec "0.1"]]
   :extra-classpath-dirs ["examples"]
   :run-aliases {:server "slacker.example.server"
                 :client "slacker.example.client"})
