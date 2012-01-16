fdddd10f5726bee109a612b680b4889be0124528
lbt05 <lbt0506@gmail.com>
2012-01-17 01:52:01 +0800

    refactor cluster test

--- test/slacker/test/cluster.clj
+++ test/slacker/test/cluster.clj
@@ -2,10 +2,10 @@
   (:use [clojure.test] )
   (:use [slacker.server cluster])
   (:use [zookeeper :as zk])
-  (:use [slacker common])
   (:require [slacker.example.api]))
 
 (def *test-conn* (zk/connect "127.0.0.1:2181"))
+(def *server-conn* (zk/connect "127.0.0.1:2181" ))
 
 
 (defn- ns-funcs [n]
@@ -20,26 +20,24 @@
   (let [cluster-map {:name "test-cluster" :node "127.0.0.1:2104" :zk "127.0.0.1:2181"}
         node-list (create-data cluster-map)
         test-conn *test-conn*
+        zk-conn *server-conn*
         ]
-    (binding [*test* true]
-      (do
-      (publish-cluster cluster-map 2104 (ns-funcs (the-ns 'slacker.example.api)))
+    (do
+      (with-zk cluster-map 2104 (ns-funcs (the-ns 'slacker.example.api)) zk-conn)
       (is (false? (every? (fn[x](empty? x))(map zk/children (repeat test-conn) node-list))))
-      (zk/close *zk-conn*)
-      ))
+      )
     ))
 
 (deftest test-setdown-cluster
   (let [cluster-map {:name "test-cluster" :node "127.0.0.1:2104" :zk "127.0.0.1:2181"}
         node-list (create-data cluster-map)
         test-conn *test-conn*
+        zk-conn *server-conn*
         ]
-    (binding [slacker.common/*test* true]
-      (do
-      (publish-cluster cluster-map 2104 (ns-funcs (the-ns 'slacker.example.api)))
-      (zk/close *zk-conn*)
+    (do
+      (zk/close zk-conn)
       (is (true? (every? (fn[x](nil? x))(map zk/children (repeat test-conn) node-list))))
       (zk/close test-conn)
-      ))
+      )
    )
-  )
+ )
