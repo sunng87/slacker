e5c355ba2743bcb83d3c070f852dcbe2497d0fa8
Sun Ning <classicning@gmail.com>
2012-01-31 20:34:20 +0800

    bump version to 0.6.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject slacker "0.5.0-SNAPSHOT"
+(defproject slacker "0.6.0-SNAPSHOT"
   :description "Clojure RPC based on serialization and TCP"
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [aleph "0.2.0"]
@@ -6,7 +6,7 @@
                  [clj-json "0.4.3"]
                  [commons-pool/commons-pool "1.5.6"]
                  [slingshot "0.10.1"]
-                 [zookeeper-clj "0.9.1"]
+                 [zookeeper-clj "0.9.2"]
                  [org.clojure/tools.logging "0.2.3"]]
   :dev-dependencies [[codox "0.3.3"]
                      [lein-exec "0.1"]]
