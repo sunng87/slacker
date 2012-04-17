ea452b84f36e6185c1a9ee80cf4b741e656094ca
Sun Ning <classicning@gmail.com>
2012-04-17 22:05:23 +0800

    update dependencies

--- project.clj
+++ project.clj
@@ -1,13 +1,13 @@
 (defproject slacker "0.8.0-SNAPSHOT"
   :description "Transparent, non-invasive RPC by clojure and for clojure"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [link "0.3.0-SNAPSHOT"]
+                 [link "0.3.0"]
                  [info.sunng/carbonite "0.2.2"]
                  [cheshire "3.0.0"]
                  [slingshot "0.10.1"]
                  [org.clojure/java.jmx "0.1"]                 
                  [org.clojure/tools.logging "0.2.3"]]
-  :dev-dependencies [[codox "0.5.0"]
+  :dev-dependencies [[codox "0.6.1"]
                      [lein-exec "0.1"]]
   :extra-classpath-dirs ["examples"]
   :run-aliases {:server "slacker.example.server"
