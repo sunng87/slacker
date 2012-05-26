12945541007f493c2033f72a239c9fe0729c62a1
Sun Ning <classicning@gmail.com>
2012-05-26 22:43:45 +0800

    upstream update

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject slacker "0.8.2"
+(defproject slacker "0.8.3-SNAPSHOT"
   :description "Transparent, non-invasive RPC by clojure and for clojure"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [link "0.3.2"]
+                 [link "0.3.3"]
                  [info.sunng/carbonite "0.2.2"]
                  [cheshire "4.0.0"]
-                 [slingshot "0.10.1"]
+                 [slingshot "0.10.2"]
                  [org.clojure/java.jmx "0.1"]                 
                  [org.clojure/tools.logging "0.2.3"]]
   :dev-dependencies [[codox "0.6.1"]
