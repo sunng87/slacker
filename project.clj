63257ac13ac8cd1968a044be073c77be89e84c09
Sun Ning <nsun@avos.com>
2012-06-18 21:14:21 +0800

    release 0.8.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject slacker "0.8.3-SNAPSHOT"
+(defproject slacker "0.8.3"
   :description "Transparent, non-invasive RPC by clojure and for clojure"
   :url "http://github.com/sunng87/slacker"
   :license {:name "Eclipse Public License"
@@ -8,13 +8,13 @@
                  [link "0.3.3"]
                  [info.sunng/carbonite "0.2.2"]
                  [cheshire "4.0.0"]
-                 [slingshot "0.10.2"]
+                 [slingshot "0.10.3"]
                  [org.clojure/java.jmx "0.1"]                 
                  [org.clojure/tools.logging "0.2.3"]]
   :profiles {:dev {:resource-paths ["examples"]
                    :dependencies [[codox "0.6.1"]]}
              :1.3 {:dependencies [org.clojure/clojure "1.3.0"]}}
   :plugins [[lein-exec "0.2.0"]]
-  :warn-on-reflection false)
+  :warn-on-reflection true)
 
 
