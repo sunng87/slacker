6f69d784191c8c3a68fe6e8c70b39e32fc1d2a6a
Sun Ning <nsun@avos.com>
2014-02-16 13:26:35 +0800

    bump version and update profile.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject slacker "0.10.0-SNAPSHOT"
+(defproject slacker "0.10.0"
   :description "Transparent, non-invasive RPC by clojure and for clojure"
   :url "http://github.com/sunng87/slacker"
   :license {:name "Eclipse Public License"
@@ -11,9 +11,9 @@
                  [slingshot "0.10.3"]
                  [org.clojure/java.jmx "0.2.0"]
                  [org.clojure/tools.logging "0.2.6"]]
-  :profiles {:dev {:resource-paths ["examples"]}
+  :profiles {:dev {:source-paths ["examples"]}
              :1.3 {:dependencies [org.clojure/clojure "1.3.0"]}}
   :plugins [[lein-exec "0.3.1"]]
   :global-vars {*warn-on-reflection* true}
-  :aliases {"run-example-server" ["run" "-m" "slacker.example.server"]
-            "run-example-client" ["run" "-m" "slacker.example.client"]})
+  :aliases {"run-example-server" ["with-profile" "default,dev" "run" "-m" "slacker.example.server"]
+            "run-example-client" ["with-profile" "default,dev" "run" "-m" "slacker.example.client"]})
