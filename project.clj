a4740d004af9f5c5bf1af2f13ad785752c25a8fd
Sun Ning <nsun@avos.com>
2014-02-18 14:02:17 +0800

    minor update on project.clj

--- project.clj
+++ project.clj
@@ -3,7 +3,6 @@
   :url "http://github.com/sunng87/slacker"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [link "0.5.4"]
                  [info.sunng/carbonite "0.2.3"]
@@ -13,7 +12,8 @@
                  [org.clojure/tools.logging "0.2.6"]]
   :profiles {:dev {:source-paths ["examples"]}
              :1.3 {:dependencies [org.clojure/clojure "1.3.0"]}}
-  :plugins [[lein-exec "0.3.1"]]
+  :plugins [[lein-exec "0.3.1"]
+            [codox "0.6.7"]]
   :global-vars {*warn-on-reflection* true}
   :aliases {"run-example-server" ["with-profile" "default,dev" "run" "-m" "slacker.example.server"]
             "run-example-client" ["with-profile" "default,dev" "run" "-m" "slacker.example.client"]})
