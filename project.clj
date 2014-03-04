f63477ee8fcbb8498b5545dea6b5232e79ff1543
Sun Ning <nsun@avos.com>
2014-03-04 22:41:55 +0800

    bump version and release 0.11.0

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject slacker "0.11.0-SNAPSHOT"
+(defproject slacker "0.11.0"
   :description "Transparent, non-invasive RPC by clojure and for clojure"
   :url "http://github.com/sunng87/slacker"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [link "0.5.6-SNAPSHOT"]
+                 [link "0.6.0"]
                  [info.sunng/carbonite "0.2.3"]
                  [cheshire "5.3.1"]
                  [org.clojure/java.jmx "0.2.0"]
