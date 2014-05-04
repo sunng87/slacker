250306dae3173d0044fb90b27b457dba2e8fa129
Sun Ning <nsun@avos.com>
2014-05-04 18:27:20 +0800

    update project.clj

--- project.clj
+++ project.clj
@@ -4,15 +4,18 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [link "0.6.5"]
+                 [link "0.6.7"]
                  [info.sunng/carbonite "0.2.3"]
                  [cheshire "5.3.1"]
                  [org.clojure/tools.logging "0.2.6"]
-                 [com.taoensso/nippy "2.6.0"]]
+                 [com.taoensso/nippy "2.6.3"]]
   :profiles {:example {:source-paths ["examples"]
                        :dependencies [[org.clojure/java.jmx "0.2.0"]]}}
   :plugins [[lein-exec "0.3.1"]
             [codox "0.6.7"]]
   :global-vars {*warn-on-reflection* true}
   :aliases {"run-example-server" ["with-profile" "default,example" "run" "-m" "slacker.example.server"]
-            "run-example-client" ["with-profile" "default,example" "run" "-m" "slacker.example.client"]})
+            "run-example-client" ["with-profile" "default,example" "run" "-m" "slacker.example.client"]}
+  :lein-release {:scm :git
+                 :deploy-via :shell
+                 :shell ["lein" "deploy" "clojars"]})
