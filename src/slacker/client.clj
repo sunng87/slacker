a31dee9fb8c02174f7559c189c9f712005b8de8a
Sun Ning <nsun@avoscloud.com>
2014-05-20 19:04:55 +0800

    fixed defremote not found

--- src/slacker/client.clj
+++ src/slacker/client.clj
@@ -59,7 +59,7 @@
 
 (defn- defn-remote*
   [sc-sym fname]
-  (eval (list 'defn-remote sc-sym (symbol fname))))
+  (eval (list 'slacker.client/defn-remote sc-sym (symbol fname))))
 
 (defn use-remote
   "import remote functions the current namespace, this function
