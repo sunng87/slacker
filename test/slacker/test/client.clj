4a55eeae59e9101b9af9c123a107ff4b54060410
Sun Ning <nsun@avoscloud.com>
2014-05-23 18:12:08 +0800

    fixed tests

--- test/slacker/test/client.clj
+++ test/slacker/test/client.clj
@@ -1,20 +1,33 @@
 (ns slacker.test.client
   (:require [clojure.test :refer :all]
-            [slacker.client.common :refer :all]))
+            [slacker.client.common :refer :all])
+  (:import [java.net ServerSocket]))
+
+(defn- open-server [port p]
+  (let [s (ServerSocket. port)]
+    (.accept s)
+    @p
+    (.close s)))
 
 (deftest test-slacker-factory
-  (let [factory (create-client-factory nil)
+  (let [sync (promise)
+        server (future (open-server 5579 sync))
+        factory (create-client-factory nil)
         addr "127.0.0.1:5579"
         client (create-client factory addr :clj {:ping-interval 10})]
     (is (= 1 (count (get-states factory))))
     (is (= addr (-> factory get-states keys first)))
-    (is (= client (-> factory (get-state addr) :refs first)))))
+    (is (= client (-> factory (get-state addr) :refs first)))
+    (deliver sync nil)))
 
 (deftest test-slacker-client
-  (let [factory (create-client-factory nil)
+  (let [sync (promise)
+        server (future (open-server 5779 sync))
+        factory (create-client-factory nil)
         addr "127.0.0.1:5779"
         client (create-client factory addr :clj {:ping-interval 10})]
     (is (= addr (server-addr client)))
     (close client)
     (is (nil? (get-state factory addr)))
-    (is (zero? (count (get-states factory))))))
+    (is (zero? (count (get-states factory))))
+    (deliver sync nil)))
