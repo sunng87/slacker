67f90d24aaf63ccc0f861676d6e5e86b69027751
Sun Ning <sunng@about.me>
2014-07-08 16:32:19 +0800

    update dependencies

--- project.clj
+++ project.clj
@@ -4,11 +4,11 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [link "0.6.9"]
+                 [link "0.6.13"]
                  [info.sunng/carbonite "0.2.3"]
                  [cheshire "5.3.1"]
-                 [org.clojure/tools.logging "0.2.6"]
-                 [com.taoensso/nippy "2.6.3"]]
+                 [org.clojure/tools.logging "0.3.0"]
+                 [com.taoensso/nippy "2.7.0-alpha1"]]
   :profiles {:example {:source-paths ["examples"]
                        :dependencies [[org.clojure/java.jmx "0.2.0"]]}}
   :plugins [[lein-exec "0.3.1"]
