0fe062fa576c07a84ce489ac38cfdf27a47baaa5
Sun Ning <sunng@about.me>
2014-09-03 15:10:35 +0800

    update dependencies
    Conflicts:
    	project.clj

--- project.clj
+++ project.clj
@@ -3,12 +3,13 @@
   :url "http://github.com/sunng87/slacker"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[link "0.6.14"]
+  :dependencies [[link "0.6.15"]
                  [cheshire "5.3.1"]
                  [org.clojure/tools.logging "0.3.0"]
-                 [com.taoensso/nippy "2.7.0-alpha1"]]
+                 [com.taoensso/nippy "2.7.0-RC1"
+                  :exclusions [org.clojure/clojure]]]
   :profiles {:example {:source-paths ["examples"]
-                       :dependencies [[org.clojure/java.jmx "0.2.0"]]}
+                       :dependencies [[org.clojure/java.jmx "0.3.0"]]}
              :clojure15 {:dependencies [[org.clojure/clojure "1.5.1"]]}
              :clojure16 {:dependencies [[org.clojure/clojure "1.6.0"]]}}
   :plugins [[lein-exec "0.3.1"]
