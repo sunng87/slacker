096616bf2f0e98796bee40c84f8111550ce285f5
Sun Ning <sunng@about.me>
2015-01-20 21:40:38 +0800

    (chore) update link to release

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://github.com/sunng87/slacker"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[link "0.7.0-SNAPSHOT"]
+  :dependencies [[link "0.7.0"]
                  [info.sunng/carbonite "0.2.3"]
                  [cheshire "5.3.1"]
                  [org.clojure/tools.logging "0.3.0"]
