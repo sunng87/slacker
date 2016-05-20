1e1720faa40898171dd8968c95ecbdff15829e42
Ning Sun <sunng@about.me>
2016-05-20 22:01:17 +0800

    Version 0.14.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject slacker "0.14.0-SNAPSHOT"
+(defproject slacker "0.14.0"
   :description "Transparent, non-invasive RPC by clojure and for clojure"
   :url "http://github.com/sunng87/slacker"
   :license {:name "Eclipse Public License"
