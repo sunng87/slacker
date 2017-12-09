19a9f5eb329cd74b4a9556992c60dc47f7334d09
Ning Sun <sunng@about.me>
2017-12-09 10:50:34 +0800

    (chore) start 0.16.0 and update to clojure 1.9
    Signed-off-by: Ning Sun <sunng@about.me>

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject slacker "0.15.2-SNAPSHOT"
+(defproject slacker "0.16.0-SNAPSHOT"
   :description "Transparent, non-invasive RPC by clojure and for clojure"
   :url "http://github.com/sunng87/slacker"
   :license {:name "Eclipse Public License"
@@ -9,7 +9,7 @@
                  [org.clojure/tools.logging "0.4.0"]]
   :profiles {:example {:source-paths ["examples"]
                        :dependencies [[org.clojure/java.jmx "0.3.4"]]}
-             :dev {:dependencies [[org.clojure/clojure "1.8.0"]
+             :dev {:dependencies [[org.clojure/clojure "1.9.0"]
                                   [cheshire "5.8.0"]
                                   [com.taoensso/nippy "2.13.0"
                                    :exclusions [org.clojure/clojure]]
@@ -19,7 +19,7 @@
              :clojure16 {:dependencies [[org.clojure/clojure "1.6.0"]]}
              :clojure17 {:dependencies [[org.clojure/clojure "1.7.0"]]}
              :clojure18 {:dependencies [[org.clojure/clojure "1.8.0"]]}
-             :clojure19 {:dependencies [[org.clojure/clojure "1.9.0-alpha10"]]}}
+             :clojure19 {:dependencies [[org.clojure/clojure "1.9.0"]]}}
   :plugins [[lein-exec "0.3.1"]
             [lein-codox "0.9.5"]]
   :global-vars {*warn-on-reflection* true}
